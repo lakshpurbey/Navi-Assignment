b3858be7ba1608bd78d3d67f65054ef827e5b585
sam purbey <you@example.com>
2022-04-23 07:25:51 +0545

    Initial Setup

---
+++ Navi Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navi Assignment
+//
+//  Created by Laksh Purbey on 23/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
